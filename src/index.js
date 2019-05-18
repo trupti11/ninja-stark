5d0359eab661c863da7624ed80e8749d712118dd
Trupti <trupti.joshi@sterlingts.com>
2019-05-18 16:12:34 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <div className="App">
+        <header className="Ninja-header">
+            <h1 className="ninja">ToDo Redux app</h1>
+          </header>
+          <App />
+        </div>
+  </Provider>
+  , document.getElementById('root'));
